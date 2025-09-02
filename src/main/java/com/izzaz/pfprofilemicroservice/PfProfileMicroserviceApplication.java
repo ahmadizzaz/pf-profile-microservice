a87a547687e5745a06d995685acd8f0a9776aa8d
Izzaz <izzaz.iqbal@gmail.com>
2025-09-02 23:05:17 +0800

    Init springboot ms
    (cherry picked from commit e23065b68f4251648d4d3c41dac0450a29e04b28)

---
+++ src/main/java/com/izzaz/pfprofilemicroservice/PfProfileMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.izzaz.pfprofilemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfProfileMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfProfileMicroserviceApplication.class, args);
+    }
+
+}
